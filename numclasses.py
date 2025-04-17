16593d8f5f2655bb792cb0dccc683e1a6fc48130
Esteban Mesa <112445347+Mesa112@users.noreply.github.com>
2025-04-17 13:14:55 -0400

    Update numclasses.py

--- numclasses.py
+++ numclasses.py
@@ -1,13 +1,6 @@
 from ultralytics import YOLO
 
-from ultralytics import YOLO
-
-# Load the pretrained YOLOv8 model (e.g., yolov8n.pt, yolov8s.pt, etc.)
 model = YOLO('yolov8n.pt')
-
-# Get the class names
 class_names = model.names
-
-# Print the class names
 for i, name in class_names.items():
     print(f"{i}: {name}")
